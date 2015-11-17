73b69f6e0da60255df9ff8abe9c48732d018dc8b
David Muñoz <damu-cazalla@hotmail.es>
2015-11-17 16:26:41 +0100

    Se han comentado las funciones de cuadrado.py
    Para el fichero cuadrado.py de la rama next se han comentado las funciones
    que tiene la clase

--- cuadrado.py
+++ cuadrado.py
@@ -1,11 +1,14 @@
 # cuadrado.py
 class cuadrado:
 	def __init__(self, x, y):
+		"""Funcion init de la clase cuadrado"""
 		self.x = x
 		self.y = y
 
 	def area(self):
+		"""Esta funcion calcula el area de un cuadrado"""
 		return self.x * self.y
 
 	def perimeter(self, x, y):
+		"""Esta funcion calcula el perimetro de un cuadrado"""
 		return (2 * self.x) + (2 * self.y)
